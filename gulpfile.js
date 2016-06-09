f199be417b65120bd7d464796333df60bb70e40b
MylesHammerschmidt <myles.hammerschmidt@banno.com>
2016-06-09 15:45:42 -0500

    Set up gulp and attempted to make gulpfile.js work.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+  connect = require('gulp-connect');
+
+gulp.task('default', function() {
+  connect();
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'http://localhost:1820',
+    livereload: true
+  });
+});
+
+gulp.task('reload', function () {
+  gulp.src('./app/*.*')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./Unit2/*.html'], ['reload']);
+});
+
+gulp.task('my-tests', function() {
+  connect.server({
+    port: 1820
+  });
+
+  connect.serverClose();
+});
+
+gulp.task('default', ['connect', 'watch']);
