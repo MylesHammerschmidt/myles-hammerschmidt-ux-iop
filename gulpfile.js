b5fe60a3b3e822873e61520df64042af66409bab
MylesHammerschmidt <myles.hammerschmidt@banno.com>
2016-06-14 15:46:37 -0500

    Sass installed. Began re-working css with Sass. Basic page structure (header) complete.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
+'use strict';
+
 var gulp = require('gulp-help')(require('gulp'));
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
 
 gulp.task('connect', 'Connects to localhost:1820', function() {
   connect.server({
@@ -14,8 +17,15 @@ gulp.task('rel', 'Reload task', function () {
     .pipe(connect.reload());
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('watch', 'Watch task', function () {
-  gulp.watch(['./**'], ['rel']);
+  gulp.watch(['./**/*.html', './**/*.css', './**/*.scss'], ['rel']);
+  gulp.watch('./**/*.scss',['sass']);
 });
 
 gulp.task('default', 'Automatically connects to localhost:1820', ['connect', 'watch']);
